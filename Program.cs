68156c26cd5564305db481e7ea5d5aeec6c80cab
GuilhermeHPSilva <guilherme.pinheirohs@gmail.com>
2021-11-25 18:44:02 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Contains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // o Contains é um método de comparação que serve para checar se existe certa letra ou palavra em uma string  
+
+            string palavra = Convert.ToString(Console.ReadLine());            
+            bool Contem = palavra.Contains("a");
+            if(Contem == true)
+            {
+                Console.WriteLine("A palavra contém a letra 'a'");
+            }
+            else
+            {
+                Console.WriteLine("A palavra não contém a letra 'a'");
+            }
+        }
+    }
+}
